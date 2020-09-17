732c34eae42662c3ce696bd4dbc238da5e1738f6
AhmedSoleiman <ahmed.soleiman.mad@gmail.com>
2020-09-17 04:20:51 +0200

    Add animations

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -28849,7 +28849,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51209" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57170" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
